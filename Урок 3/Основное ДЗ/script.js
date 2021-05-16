87276bb39cc8893a6cc107caf2fe2e51d72a72d3
Levingt <MURD94@yandex.ru>
2021-05-16 08:47:07 +0300

    Course

---
+++ Урок 3/Основное ДЗ/script.js
@@ -0,0 +1,36 @@
+let money = prompt(`Ваш бюджет?`);
+let shopName = prompt(`Название вашего магазина?`);
+let myShop = {
+    Money: money,
+    ShopName: shopName,
+    shopProduct: [],
+    personal: [],
+    open: false,
+};
+
+for (let i = 0; i < 5; i++) {
+    let product = prompt(`Что будем продавать?`);
+    if (isNaN(product)) {
+        if (typeof product !== null && product !== "" && product.length < 10)
+        {myShop.shopProduct[i] = product;
+    } else {
+        i--;
+    }
+    } else {
+        i--;
+    }
+}
+console.log(myShop);
+
+if (time < 0) {
+    console.log("Такого не может быть!");
+}
+else if (time > 8 && time < 20) {
+    console.log("Время работать!");
+}
+else if (time < 24) {
+    console.log("Мы не работаем");
+}
+else {
+    console.log("В сутках только 24 часа");
+}
